e2c9c363335951faf152a552759a353a6588c916
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-11-17 14:06:02 +0530

    Inserting contacts using threading

--- AddressBookDay27/Program.cs
+++ AddressBookDay27/Program.cs
@@ -5,7 +5,9 @@
 // <creator Name="Nikhil Kumar yadav"/>
 // --------------------------------------------------------------------------------------------------------------------
 
+
 using System;
+using System.Collections.Generic;
 
 namespace AddressBook_AdoNet
 {
@@ -18,7 +20,25 @@ namespace AddressBook_AdoNet
         {
             Console.WriteLine("Welcome To Address Book AdoNet");
             AddressBookOperations addressBookOperations = new AddressBookOperations();
-            Console.WriteLine(addressBookOperations.GetContactDetailsBetweenDateRange() ? "Query Succesful " : "Failed");
+            ContactDetails contactDetails = new ContactDetails();
+            contactDetails.firstName = "AAYAM";
+            contactDetails.lastName = "kumar";
+            contactDetails.phoneNumber = 877655447;
+            contactDetails.email = "aa@gmail.com";
+            contactDetails.city = "kol";
+            contactDetails.start = Convert.ToDateTime("1/2/2018");
+            contactDetails.state = "wb";
+            contactDetails.addressBookId = 6;
+            contactDetails.completeAddressId = 106;
+            contactDetails.addressBookName = "First";
+            contactDetails.address = "street 1";
+            contactDetails.zip = 315151;
+            contactDetails.typeId = 1;
+            contactDetails.typeName = "Friends";
+            List<ContactDetails> listOfContactsToBeAdded = new List<ContactDetails>();
+            listOfContactsToBeAdded.Add(contactDetails);
+            addressBookOperations.AddingMultipleContactDetailsUsingThreading(listOfContactsToBeAdded);
+
         }
     }
 }
